29a208eb862dcf634a2058725fbc11a1ff97abaf
ChehAchraf <achrafchehboun@outlook.com>
2025-02-03 11:37:44 +0100

    first commit

---
+++ app/controller/Pages.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controllers;
+
+class Pages
+{
+    public function index()
+    {
+        echo "Welcome to the default page!";
+    }
+    public function about(){
+
+    }
+}
---
+++ app/controller/Post.php
@@ -0,0 +1,10 @@
+<?php
+class Post
+{
+    public function index(){
+        echo "Post";
+    }
+    public function show(){
+        echo "Show";
+    }
+}
